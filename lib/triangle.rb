b2965f6d048d8bcbfd77b253c998b22067b533b2
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-11 15:44:00 -0400

    Implement error for triangle class

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 class Triangle
-  # write code here
+  def initialize side_one, side_two, side_three
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def kind
+    if @sides.filter{|side| side < 0}.length > 0 ||
+      @sides[0] >= @sides[1] + @sides[2] ||
+      @sides[1] >= @sides[0] +  @sides[2] ||
+      @sides[2] >= @sides[0] + @sides[1]
+      raise TriangleError
+    elsif @sides.uniq.length == 3
+      :scalene
+    elsif @sides.uniq.length == 2
+      :isosceles
+    elsif @sides.uniq.length == 1
+      :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
